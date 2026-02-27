d3d9572527dd065159baad90c6755374a744ccbf
PauloQuagliatto <pquagliatto.dev@hotmail.com>
2026-02-27 18:04:32 -0300

    improving hypr config

--- .config/hypr/scripts/launcher.sh
+++ .config/hypr/scripts/launcher.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/env bash
 
 # -----------------------------------------------------
-# Load Launcher
+# Load Rofi Launcher
 # -----------------------------------------------------
-launcher=$(cat $HOME/.config/ml4w/settings/launcher)
-
-# Use Walker
-_launch_walker() {
-    $HOME/.config/walker/launch.sh --height 500
-}
-
-# Use Rofi
-_launch_rofi() {
-    pkill rofi || rofi -show drun -replace -i  
-}
-
-if [ "$launcher" == "walker" ]; then
-    _launch_walker
-else
-    _launch_rofi
-fi
+pkill rofi || rofi -show drun -icon-theme "Tela dark" -show-icons -replace -i :
--- .config/paulo/settings/rofi_launcher.sh
+++
@@ -1,1 +0,0 @@
-pkill rofi || rofi -show drun -icon-theme "Tela dark" -show-icons -replace -i :
