a944c759697a778cf8ea3443233d2ccddc0d0a71
PauloQuagliatto <pquagliatto.dev@hotmail.com>
2026-03-03 14:01:40 +0000

    waybar config and toggle working correctly

--- .config/waybar/toggle.sh
+++ .config/waybar/toggle.sh
@@ -7,9 +7,9 @@
 #            |___/ |___/                         |___/
 #
 
-if [ -f $HOME/.config/ml4w/settings/waybar-disabled ]; then
-    rm $HOME/.config/ml4w/settings/waybar-disabled
+if [ -f $HOME/.config/paulo/settings/waybar-disabled ]; then
+    rm $HOME/.config/paulo/settings/waybar-disabled
 else
-    touch $HOME/.config/ml4w/settings/waybar-disabled
+    touch $HOME/.config/paulo/settings/waybar-disabled
 fi
 $HOME/.config/waybar/launch.sh &
